b745083ff483450eca15970ce3fca218e526c66e
NItin-36703 <nitin616pal@gmail.com>
2026-08-17 14:48:18 +0530

    Time: 86 ms (6.56%), Space: 44.3 MB (35.63%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+   int n=arr.size();
+    vector<int> copy(n);
+    for (int i=0;i<n;i++){
+        copy[i] = arr[i];
+    } 
+    sort(copy.begin(),copy.end());
+    map<int,int>mp;
+   int rank = 1;
+
+for (int i=0;i<n;i++){
+    if(mp.find(copy[i]) != mp.end()){
+        continue;
+    }
+    mp[copy[i]] = rank;
+    rank++;
+}
+    vector<int> ans(n);
+    for (int i=0;i<n;i++){
+        ans[i]= mp[arr[i]] ;
+    }
+   return ans; }
+};
+    
