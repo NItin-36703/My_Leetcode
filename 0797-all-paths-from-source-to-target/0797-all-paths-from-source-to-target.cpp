be13db4e645aef916ced946bd52b356f1c5f6268
NItin-36703 <nitin616pal@gmail.com>
2026-09-26 14:25:06 +0530

    Time: 15 ms (17.69%), Space: 19.5 MB (15.28%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
 void solve(vector<vector<int>>& ans, vector<vector<int>>&graph, vector<int> temp,int i){
- if (i == graph.size()-1){
+
     temp.push_back(i);
+
+ if (i == graph.size()-1){
+  
     ans.push_back(temp);
     return;
  }
-    queue<int>q;
-    q.push(i);
-    while (!q.empty()){
-        temp.push_back(q.front());
-        q.pop();
+   
 
         for(auto it: graph[i]){
             solve(ans,graph,temp,it);
         }
-    }
+    
 }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
       vector<vector<int>> ans;
